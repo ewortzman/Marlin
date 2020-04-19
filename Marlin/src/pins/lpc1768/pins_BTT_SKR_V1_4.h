0a6a6490d8ca36cf7eb0e3982826f2464528ef1a
Ethan Wortzman <ethan.wortzman@gmail.com>
2020-04-18 20:18:24 -0400

    swap therm0 and therm1

--- Marlin/src/pins/lpc1768/pins_BTT_SKR_V1_4.h
+++ Marlin/src/pins/lpc1768/pins_BTT_SKR_V1_4.h
@@ -148,7 +148,8 @@
   #define E1_CS_PIN        P1_01
 #endif
 
-#define TEMP_1_PIN         P0_23_A0   // A2 (T2) - (69) - TEMP_1_PIN
+#define TEMP_0_PIN         P0_23_A0   // A1 (T1) - (68) - TEMP_0_PIN
+#define TEMP_1_PIN         P0_24_A1   // A2 (T2) - (69) - TEMP_1_PIN
 #define TEMP_BED_PIN       P0_25_A2   // A0 (T0) - (67) - TEMP_BED_PIN
 
 //
